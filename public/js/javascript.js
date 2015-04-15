ee9e57ba0dd14298ec404000621f7f9024e3e55c
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-15 15:20:33 -0400

    dif format

---
+++ public/js/javascript.js
@@ -0,0 +1,15 @@
+function startLightBox(){
+	var lbBg = document.getElementById("lightBoxBg");
+	var lb = document.getElementById("lightBox");
+
+	lgBg.style.display = "block";
+	lb.style.display = "block";
+}
+
+function dismiss() {
+	var lbBg = document.getElementById("lightBoxBg");
+	var lb = document.getElementById("lightBox");
+
+	lgBg.style.display = "none";
+	lb.style.display = "none";
+}
